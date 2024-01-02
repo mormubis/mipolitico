d5cf2b30cd1e15de6706b2a7ee84ca4b0f91dfc7
Adrian de la Rosa <adelarosabretin@coursera.org>
2024-01-01 17:35:37 -0800

    feat: add nx

--- apps/person/src/moleculer.config.ts
+++ apps/person/src/moleculer.config.ts
@@ -4,30 +4,41 @@ const options: BrokerOptions = {
   // Concurrency
   bulkhead: { enabled: true, concurrency: 10, maxQueueSize: 100 },
 
+  // Enable built-in cache
   cacher: { type: 'Memory', options: { ttl: 60 } },
 
+  // Enable circuit breaker
+  // @see https://moleculer.services/docs/0.14/fault-tolerance#Circuit-Breaker
   circuitBreaker: { enabled: true },
 
+  // Log level for built-in console logger.
   logLevel: 'warn',
 
+  // Depth of calling
   maxCallLevel: 25,
 
+  // Enable metrics
   metrics: {
     enabled: true,
     reporter: 'Console',
   },
 
+  // Namespace of nodes to segment your nodes
+  // In case I want to run other applications over the same NATS broker
   namespace: 'congress',
 
   // Load balancing strategy
   registry: { strategy: 'CpuUsage' },
 
+  // Request timeout
   requestTimeout: 10 * 1_000, // 10s
 
+  // Enable retries
   retryPolicy: {
     enabled: false,
   },
 
+  // CBOR Serializer
   serializer: 'CBOR',
 
   tracing: {
@@ -45,6 +56,7 @@ const options: BrokerOptions = {
     enabled: false,
   },
 
+  // Enable validation of schema
   validator: true,
 };
 
