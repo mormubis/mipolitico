429673571d166f7cbc66f9689c965ee6a3c153ea
Adrian de la Rosa <adelarosabretin@coursera.org>
2024-01-07 13:00:13 -0800

    fix(person): Dockerfile to use pnpm

---
+++ apps/group/src/index.ts
@@ -0,0 +1,21 @@
+//fetch("https://www.congreso.es/es/grupos/composicion-en-la-legislatura?p_p_id=grupos&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=gruposSearch&p_p_cacheability=cacheLevelPage", {
+//   "headers": {
+//     "accept": "application/json, text/javascript, */*; q=0.01",
+//     "accept-language": "en-US,en;q=0.5",
+//     "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+//     "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Brave\";v=\"120\"",
+//     "sec-ch-ua-mobile": "?0",
+//     "sec-ch-ua-platform": "\"macOS\"",
+//     "sec-fetch-dest": "empty",
+//     "sec-fetch-mode": "cors",
+//     "sec-fetch-site": "same-origin",
+//     "sec-gpc": "1",
+//     "x-requested-with": "XMLHttpRequest"
+//   },
+//   "referrer": "https://www.congreso.es/es/grupos/composicion-en-la-legislatura",
+//   "referrerPolicy": "strict-origin-when-cross-origin",
+//   "body": "_grupos_currentLegislatura=15&_grupos_idLegislatura=XV",
+//   "method": "POST",
+//   "mode": "cors",
+//   "credentials": "include"
+// });
