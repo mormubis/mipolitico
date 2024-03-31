b77e50f5ce8620f04f4b1cab3053322d6d177bd2
Adrian de la Rosa <adelarosabretin@coursera.org>
2024-03-31 18:39:01 -0400

    feat: group microservice ready

--- apps/group/src/services/group.service.ts
+++ apps/group/src/services/group.service.ts
@@ -1,10 +1,129 @@
 import { ServiceSchema } from 'moleculer';
+import { romanize } from 'romans';
+
+type Group = {
+  listaDir: {
+    descripcion: string;
+    direccion: string;
+    idTipo: number;
+    orden: number;
+    tieneImagen: boolean;
+    secuencial: number;
+    idGrupo: number;
+  }[];
+  nombreGrupo: string;
+  gprDescAbr: string;
+  grpDesc: string;
+  codOrg: number;
+  numMiembros: number;
+  idLegislatura: number;
+  fechaConstitucion: string;
+};
+
+type GroupResponse = {
+  data: Group[];
+};
+
+const ALIASES = {
+  'CC': [
+    'Coalición Canaria',
+    'Coalición Canaria-Nueva Canarias',
+    'Grupo Parlamentario de Coalición Canaria',
+  ],
+  'CDS': ['Grupo Parlamentario CDS', 'Grupo Parlamentario de CDS'],
+  'CiU': [
+    'Catalán  (CiU)',
+    'Catalán (CiU)',
+    'Grupo Parlamentario Catalán (Convergencia i Unió)',
+    'Grupo Parlamentario Catalán (Convergència i Unió)',
+  ],
+  'EAJ-PNV': [
+    'Grupo Parlamentario Vasco (EAJ-PNV)',
+    'Grupo Parlamentario Vasco (P.N.V.)',
+    'Grupo Parlamentario Vasco (PNV)',
+    'Grupo Vasco (PNV)',
+    'Vasco (EAJ-PNV)',
+  ],
+  'ERC': [
+    'Esquerra Republicana (ERC)',
+    'Esquerra Republicana - Izquierda Unida-ICV',
+    'Esquerra Republicana',
+    'Republicano',
+  ],
+  'IU': [
+    'Federal Izquierda Unida',
+    'Grupo Parlamentario Federal Izquierda Unida-Iniciativa per Catalunya',
+    'Grupo Parlamentario Federal de Izquierda Unida',
+    'Grupo Parlamentario Mixto - Agrupación IU - IC',
+    'Grupo Parlamentario de Izquierda Unida-Iniciativa per Catalunya',
+    'Izquierda Unida - ICV',
+    'La Izquierda Plural',
+  ],
+  'JxCat': ['Junts per Catalunya'],
+  'Mixto': ['Grupo Parlamentario Mixto'],
+  'PP': [
+    'Grupo Parlamentario Coalición Popular',
+    'Grupo Parlamentario Popular del Congreso',
+    'Grupo Parlamentario Popular en el Congreso',
+    'Grupo Parlamentario de Coalición Popular',
+    'Popular en el Congreso',
+    'Popular',
+  ],
+  'PSOE': [
+    'Grupo Parlamentario Socialista del Congreso',
+    'Grupo Parlamentario Socialista',
+    'Grupo Socialista del Congreso',
+    'Partido Socialista Obrero Español',
+    'Socialista',
+  ],
+  'Podemos': ['Podemos-En Comú Podem-En Marea'],
+  'SUMAR': ['Plurinacional SUMAR'],
+  'UPyD': ['Unión Progreso y Democracia'],
+  'Unidas Podemos': [
+    'Confederal de Unidas Podemos-En Comú Podem-Galicia en Común',
+    'Podemos-En Comú Podem-En Marea',
+  ],
+  'Unidos Podemos': ['Confederal de Unidos Podemos-En Comú Podem-En Marea'],
+};
+
+const NAMES = {
+  'CC': 'Coalición Canaria',
+  'CDS': 'Centro Democrático y Social',
+  'CiU': 'Convergència i Unió',
+  'EAJ-PNV': 'Partido Nacionalista Vasco',
+  'ERC': 'Esquerra Republicana de Catalunya',
+  'IU': 'Izquierda Unida',
+  'JxCat': 'Junts per Catalunya',
+  'Mixto': 'Mixto',
+  'PP': 'Partido Popular',
+  'PSOE': 'Partido Socialista Obrero Español',
+  'SUMAR': 'SUMAR',
+  'UPyD': 'Unión Progreso y Democracia',
+  'Unidas Podemos': 'Unidas Podemos',
+  'Unidos Podemos': 'Unidos Podemos',
+};
+
+const MAPPER = Object.entries(ALIASES).reduce(
+  (acc, [id, names]) => {
+    names.forEach((name) => {
+      acc[name] = id;
+    });
+
+    return acc;
+  },
+  {} as Record<string, string>,
+);
+
+function hasName(name: string): name is keyof typeof NAMES {
+  return name in NAMES;
+}
 
 const service: ServiceSchema = {
   actions: {
     get: {
-      async handler({ params }) {
-        const { id, legislature, name } = params ?? {};
+      cache: true,
+      async handler(context) {
+        const { id, legislature = 15, name } = context.params ?? {};
 
         const response = await fetch(
           'https://www.congreso.es/es/grupos/composicion-en-la-legislatura?p_p_id=grupos&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=gruposSearch&p_p_cacheability=cacheLevelPage',
@@ -16,19 +135,59 @@ const service: ServiceSchema = {
               'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
               'x-requested-with': 'XMLHttpRequest',
             },
-            body: '_grupos_currentLegislatura=15&_grupos_idLegislatura=XV',
+            body: new URLSearchParams({
+              _grupos_idLegislatura: romanize(legislature),
+            }),
             method: 'POST',
           },
         );
 
-        const data = await response.json();
+        const { data } = (await response.json()) as GroupResponse;
 
-        return data;
+        return await Promise.all(
+          data.map(async (group) => {
+            const id = await context.call('group.id', { name: group.nombreGrupo });
+
+            return {
+              id,
+              name: await context.call('group.name', { id }),
+              description: group.grpDesc,
+              links: (group.listaDir || [])?.map((link) => ({
+                description: link.descripcion,
+                value: link.direccion,
+              })),
+            };
+          }),
+        );
       },
       params: {
         id: 'string|optional',
         legislature: 'number|optional',
         name: 'string|optional',
+      },
+    },
+
+    id: {
+      cache: true,
+      handler(context) {
+        const { name } = context.params;
+
+        return MAPPER[name] ?? name;
+      },
+      params: {
+        name: 'string',
+      },
+    },
+
+    name: {
+      cache: true,
+      handler(context) {
+        const { id } = context.params;
+
+        return hasName(id) ? NAMES[id] : id;
+      },
+      params: {
+        id: 'string',
       },
     },
   },
